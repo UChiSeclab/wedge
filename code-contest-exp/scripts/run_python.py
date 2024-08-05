e25ddab29d835d6531cc5b0b606dedf909f20d1d
Jun <jy70@illinois.edu>
2024-08-05 16:33:19 -0500

    minor: move legacy result into a folder

---
+++ code-contest-exp/scripts/run_python.py
@@ -0,0 +1,12 @@
+import os
+import subprocess
+import json
+import re
+import time
+from queue import Queue
+import threading
+from scripts import *
+from tqdm import tqdm
+
+def run_python(python_solution_dir, class_name, idir, odir, time_limit=1):
+    pass
