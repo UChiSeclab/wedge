57fe5ee5fdfec6c6cafe10efed820cb8ed42706b
Cheng-Chi Wang <casperwang@goku.cs.uchicago.edu>
2024-08-08 23:28:48 -0500

    add run_cpp.py

---
+++ code-contest-exp/scripts/analysis.py
@@ -0,0 +1,36 @@
+import json
+
+# Reading the JSON data from a file
+file_path = './java_result.json'
+with open(file_path, 'r', encoding='utf-8') as file:
+    data = json.load(file)
+
+cnt = 0
+for problem_name in data.keys():
+    max_time_solution = (0, "")
+    min_time_solution = (20, "")
+    problem = data[problem_name]
+    total_time = 0
+    for solution_name in problem.keys():
+        if "solutions" not in solution_name:
+            continue
+        solution = problem[solution_name]
+        incorrect_flag = solution["online_judge_verdict"] == "incorrect"
+        for verdict in solution["verdict"]:
+            incorrect_flag |= verdict != "AC"
+        if incorrect_flag:
+            continue
+        average_time = 0
+        for test_name in solution["time_dict"]:
+            average_time += sum(solution["time_dict"][test_name]) / 5
+        average_time /= len(solution["time_dict"])
+        total_time += average_time
+        max_time_solution = max(max_time_solution, (average_time, solution_name))
+        min_time_solution = min(min_time_solution, (average_time, solution_name))
+    if max_time_solution[0] / min_time_solution[0] >= 1.6:
+        print()
+        print(problem_name)
+        print(min_time_solution)
+        print(max_time_solution)
+        cnt += 1
+print(cnt)
--- code-contest-exp/scripts/run_cpp.py
+++ code-contest-exp/scripts/run_cpp.py
@@ -1,7 +1,6 @@
-"""Run java solutions on the tests."""
+"""Run cpp solutions on the tests."""
 import os
 import subprocess
-import re
 from pathlib import Path
 import random
 import json
@@ -15,26 +14,22 @@ from common import Language
 from config import config
 from utils import get_cf_problems, filter_problems
 
-def compile_java(solution_dir: Path, solution_code: str):
-    """Compiles java solution.
+def compile_cpp(solution_dir: Path, solution_code: str):
+    """Compiles C++ solution.
     
     Args:
     - solution_dir (Path): The directory to store the solution.
-    - solution_code (str): The java solution code
+    - solution_code (str): The C++ solution code
     """
-    class_match = re.search(r"public\s+class\s+(\w+)", solution_code)
-    if not class_match:
-        return "error"
-    class_name = class_match.group(1)
-    file_path = solution_dir / f"{class_name}.java"
+    file_path = solution_dir / "solution.cpp"
     with open(file_path, 'w', encoding='utf-8') as file:
         file.write(solution_code)
-    compile_process = subprocess.run(["javac", file_path], capture_output=True, check=False)
+    compile_process = subprocess.run(["g++", "-std=c++17", file_path, "-o", solution_dir / "solution"], capture_output=True, check=False)
     if compile_process.returncode != 0:
         return "error"
-    return class_name
+    return "solution"
 
-def run_java(
+def run_cpp(
         solution_dir: Path,
         solution_file_name: str,
         input_dir: Path,
@@ -43,27 +38,26 @@ def run_java(
         time_limit: float = 1,
         repeat_test_idx: int = 0,
         ):
-    """Runs java solution on tests.
+    """Runs C++ solution on tests.
     
     Args:
-        soltuion_dir (Path): The directory of the solution.
-        solution_file_name (str): The file name of the solution.
+        solution_dir (Path): The directory of the solution.
+        solution_file_name (str): The file name of the executable.
         input_dir (Path): The directory which stores input files.
         output_dir (Path): The directory which stores output files.
         write_output (bool): Whether to write the output to the output_dir or not.
         time_limit (float): The actual time limit for this problem.
         repeat_test_idx (int): The index of repeat test.
-
-    Returns:
-        test_results (Dict): A dictionary to store the test results.
-            verdict: "AC" / "WA" / "TLE" / "KILL"
     """
 
     tmp_dir = Path(f"{solution_file_name}_{repeat_test_idx}")
     tmp_dir.mkdir(exist_ok=True)
-    with open(solution_dir / solution_file_name, 'r', encoding='utf-8') as file:
+    with open(solution_dir / f"{solution_file_name}", 'r', encoding='utf-8') as file:
         solution_code = file.read()
-    class_name = compile_java(tmp_dir, solution_code)
+    executable_name = compile_cpp(tmp_dir, solution_code)
+    if executable_name == "error":
+        shutil.rmtree(tmp_dir)
+        return
 
     max_time = 0
     test_cnt = 0
@@ -77,7 +71,7 @@ def run_java(
             try:
                 start_time = time.time()
                 run_process = subprocess.run(
-                    ["java", "-Xmx512m", "-cp", tmp_dir, class_name],
+                    [tmp_dir / executable_name],
                     stdin=input_file,
                     stdout=subprocess.PIPE,
                     stderr=subprocess.PIPE,
@@ -86,15 +80,18 @@ def run_java(
                 )
                 runtime = time.time() - start_time
                 if run_process.returncode == 0 and run_process.stdout:
-                    if not write_output:
+                    try:
                         program_output = run_process.stdout.decode('utf-8').split()
-                        with open(output_path, 'r', encoding='utf-8') as file:
-                            actual_output = file.read().split()
-                        if actual_output != program_output:
-                            wrong_answer_flag = True
-                    else:
-                        with open(output_path, 'w', encoding='utf-8') as file:
-                            file.write(run_process.stdout.decode('utf-8'))
+                        if not write_output:
+                            with open(output_path, 'r', encoding='utf-8') as file:
+                                actual_output = file.read().split()
+                            if actual_output != program_output:
+                                wrong_answer_flag = True
+                        else:
+                            with open(output_path, 'w', encoding='utf-8') as file:
+                                file.write(run_process.stdout.decode('utf-8'))
+                    except UnicodeDecodeError:
+                        wrong_answer_flag = True
             except subprocess.TimeoutExpired:
                 runtime = config['max_time_limit']
         max_time = max(max_time, runtime)
@@ -119,16 +116,16 @@ def run_java(
         test_result['verdict'] = 'TLE'
     return test_result
 
-def run_java_wrapper(args):
+def run_cpp_wrapper(args):
     """Wrapper function to unpack arguments."""
-    return run_java(*args)
+    return run_cpp(*args)
 
 def main(
     output_file: str = config["output_file"],
     experiment_name: str = config["experiment_name"],
     problem_root_dir: str = config["problem_root_dir"],
 ):
-    """Runs all java solutions in the folder"""
+    """Runs all C++ solutions in the folder"""
     problem_root_dir = Path(problem_root_dir)
     filtered_problems = filter_problems(get_cf_problems())
 
@@ -145,7 +142,7 @@ def main(
             experiment_dir = problem_dir
         else:
             experiment_dir = problem_dir / experiment_name
-        solution_dir = problem_dir / "solutions" / "java"
+        solution_dir = problem_dir / "solutions" / "cpp"
         input_dir = experiment_dir / "input"
         output_dir = experiment_dir / "output"
         time_limit = problem["time_limit"]["seconds"] + problem["time_limit"]["nanos"] / 10**9
@@ -157,7 +154,7 @@ def main(
         test_args = []
         for sol_type in ["solutions", "incorrect_solutions"]:
             for solution_idx, _ in enumerate(problem[sol_type]["solution"]):
-                if Language.idx_to_lang(problem[sol_type]["language"][solution_idx]) != str(Language.JAVA):
+                if Language.idx_to_lang(problem[sol_type]["language"][solution_idx]) != str(Language.CPP):
                     continue
                 solution_file_name = f"{sol_type}_{solution_idx:03}"
                 for idx in range(config["repeat_test"]):
@@ -168,19 +165,21 @@ def main(
 
         max_workers = max(1, int(0.75 * os.cpu_count()))
         with Pool(processes=max_workers) as pool:
-            res = list(tqdm(pool.imap(run_java_wrapper, test_args), total=len(test_args)))
+            res = list(tqdm(pool.imap(run_cpp_wrapper, test_args), total=len(test_args)))
         for idx, test_arg in enumerate(test_args):
+            if not res[idx]:
+                continue
             online_judge_verdict = "incorrect" if "incorrect" in test_arg[1] else "correct"
             if test_arg[1] not in results[problem["name"]]:
                 results[problem["name"]][test_arg[1]] = {
-                    "language": "java",
+                    "language": "cpp",
                     "online_judge_verdict": online_judge_verdict,
                     "verdict": [],
                     "average_time": [],
                     "max_time": [],
                     "time_dict": {}
                 }
-            results[problem["name"]][test_arg[1]]["language"] = "java"
+            results[problem["name"]][test_arg[1]]["language"] = "cpp"
             results[problem["name"]][test_arg[1]]["online_judge_verdict"] = online_judge_verdict
             results[problem["name"]]["time_limit"] = test_arg[5]
             results[problem["name"]][test_arg[1]]["verdict"].append(res[idx]['verdict'])
