211ae39d7dd7a97e9a50f01b592fbaa506d5d253
Jun <jy70@illinois.edu>
2024-09-10 22:11:29 -0500

    feat: add violin plot

--- code-contest-exp/scripts/statistics.py
+++ code-contest-exp/scripts/statistics.py
@@ -11,47 +11,89 @@ from config import config
 from utils import filter_problems, get_cf_problems
 from utils import squeeze_time_dict
 
-def plot_avg_over_problem(problem_statistics: Dict, strategies: List[str]):
-    # Prepare data for plotting
+def get_time_values(problem_statistics: Dict, strategies: List[str]):
     problems = list(problem_statistics.keys())
     languages = set(lang for prob in problem_statistics.values() for lang in prob.keys())
 
     avg_time_values = {} # strategy -> List[float]
     max_time_values = {} # strategy -> List[float]
-    var_time_values = {} # strategy -> List[float]
-    
+    cv_time_values = {} # strategy -> List[float]
+
     for problem_id in problems:
         for language in languages:
             avg_times = []
             max_times = []
-            var_times = []
+            cv_times = []
             for strategy in strategies:
                 strategy_data = problem_statistics.get(problem_id, {}).get(language, {}).get(strategy, (0, 0, 0))
                 avg_times.append(strategy_data[0])
                 max_times.append(strategy_data[1])
-                var_times.append(strategy_data[2])
+                cv_times.append(strategy_data[2])
 
             if any(time == 0 for time in avg_times) or any(time == 0 for time in max_times):
                 print(f"[Warning] {problem_id} with {language} has 0 time")
                 continue
-            
+
             for strategy in strategies:
                 avg_time_values[strategy] = avg_time_values.get(strategy, [])
                 max_time_values[strategy] = max_time_values.get(strategy, [])
-                var_time_values[strategy] = var_time_values.get(strategy, [])
+                cv_time_values[strategy] = cv_time_values.get(strategy, [])
                 avg_time_values[strategy].append(avg_times[strategies.index(strategy)])
                 max_time_values[strategy].append(max_times[strategies.index(strategy)])
-                var_time_values[strategy].append(var_times[strategies.index(strategy)])
+                cv_time_values[strategy].append(cv_times[strategies.index(strategy)])
+
+    return avg_time_values, max_time_values, cv_time_values
+
+def plot_violin_plot(problem_statistics: Dict, strategies: List[str]):
+    # Prepare data for plotting
+    avg_time_values, max_time_values, cv_time_values = get_time_values(problem_statistics, strategies)
     
+    # Define plot characteristics
+    n_groups = len(strategies)
+    index = np.arange(n_groups)
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(15, 15))
+    
+    # Plot avg time
+    ax1.violinplot(avg_time_values.values(), showmeans=True)
+    ax1.set_xlabel('Strategy')
+    ax1.set_ylabel('Average Time')
+    ax1.set_title('Average Time by Strategy')
+    ax1.set_xticks(index)
+    ax1.set_xticklabels(strategies, ha='center', rotation=45)
+
+    # Plot max time
+    ax2.violinplot(max_time_values.values(), showmeans=True)
+    ax2.set_xlabel('Strategy')
+    ax2.set_ylabel('Max Time')
+    ax2.set_title('Max Time by Strategy')
+    ax2.set_xticks(index)
+    ax2.set_xticklabels(strategies, ha='center', rotation=45)
+
+    # Plot Coefficient of Variation
+    ax3.violinplot(cv_time_values.values(), showmeans=True)
+    ax3.set_xlabel('Strategy')
+    ax3.set_ylabel('Coefficient of Variation')
+    ax3.set_title('Coefficient of Variation by Strategy')
+    ax3.set_xticks(index)
+    ax3.set_xticklabels(strategies, ha='center', rotation=45)
+
+    # Adjust layout to prevent overlapping
+    plt.tight_layout()
+    plt.savefig("./results/distribution.png")
+
+def plot_avg_over_problem(problem_statistics: Dict, strategies: List[str]):
+    # Prepare data for plotting
+    avg_time_values, max_time_values, cv_time_values = get_time_values(problem_statistics, strategies)
+
     avg_avg_time_values = {strategy: mean(times) for strategy, times in avg_time_values.items()}
     avg_max_time_values = {strategy: mean(times) for strategy, times in max_time_values.items()}
-    avg_var_time_values = {strategy: mean(times) for strategy, times in var_time_values.items()}
+    avg_cv_time_values = {strategy: mean(times) for strategy, times in cv_time_values.items()}
     # Define plot characteristics
     n_groups = len(strategies)
     bar_width = 0.20
     index = np.arange(n_groups)
-    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(15, 15))
-    
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(20, 15))
+
     ax1.bar(index, avg_avg_time_values.values(), bar_width, label='avg_time', color='blue')
     ax1.set_ylabel('Average Time')
     ax1.set_title('Average "Average Time" by Strategy')
@@ -65,8 +107,8 @@ def plot_avg_over_problem(problem_statistics: Dict, strategies: List[str]):
     ax2.set_xticks(index)
     ax2.set_xticklabels(strategies, ha='center', rotation=45)
     ax2.legend()
-    
-    ax3.bar(index, avg_var_time_values.values(), bar_width, label='var_time', color='green')
+
+    ax3.bar(index, avg_cv_time_values.values(), bar_width, label='cv_time', color='green')
     ax3.set_ylabel('Coefficient of Variation')
     ax3.set_title('Average "Coefficient of Variation" by Strategy')
     ax3.set_xticks(index)
@@ -81,7 +123,7 @@ def plot_avg_over_problem(problem_statistics: Dict, strategies: List[str]):
 def plot_problem_statistics(
     problem_statistics: Dict,
     strategies: List[str],
-    colors:List[str] = ['purple', 'green', 'red', 'yellow', 'orange', 'blue', 'cyan', 'magenta', 'pink', 'brown', 'gray', 'olive', 'lime', 'teal', 'navy']
+    colors:List[str] = ['purple', 'green', 'red', 'yellow', 'orange', 'blue', 'cyan', 'magenta', 'pink', 'brown', 'gray', 'olive', 'lime', 'teal', 'navy', 'maroon']
 ):
     # Prepare data for plotting
     problems = list(problem_statistics.keys())
@@ -93,39 +135,39 @@ def plot_problem_statistics(
     x_labels = []
     avg_time_values = []
     max_time_values = []
-    var_time_values = []
+    cv_time_values = []
 
     for problem_id in problems:
         for language in languages:
             avg_times = []
             max_times = []
-            var_times = []
+            cv_times = []
             for strategy in strategies:
                 strategy_data = problem_statistics.get(problem_id, {}).get(language, {}).get(strategy, (0, 0, 0))
                 avg_times.append(strategy_data[0])
                 max_times.append(strategy_data[1])
-                var_times.append(strategy_data[2])
-            if any(time == 0 for time in avg_times + max_times + var_times):
+                cv_times.append(strategy_data[2])
+            if any(time == 0 for time in avg_times + max_times + cv_times):
                 print(f"[Warning] {problem_id} with {language} has 0 time")
                 continue
             label = f"{problem_id}-{language}"
             x_labels.append(label)
             avg_time_values.append(avg_times)
             max_time_values.append(max_times)
-            var_time_values.append(var_times)
+            cv_time_values.append(cv_times)
 
     # Convert to arrays for easier manipulation
     avg_time_values = np.array(avg_time_values)
     max_time_values = np.array(max_time_values)
-    var_time_values = np.array(var_time_values)
+    cv_time_values = np.array(cv_time_values)
 
     # Define plot characteristics
     n_groups = len(x_labels)
-    bar_width = 0.08
+    bar_width = 0.06
     index = np.arange(n_groups)
 
     # Plot avg time
-    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(20, 15))
+    fig, (ax1, ax2, ax3) = plt.subplots(nrows=3, ncols=1, figsize=(25, 15))
 
     for i in range(len(strategies)):
         ax1.bar(index + i * bar_width, avg_time_values[:, i], bar_width, label=strategies[i], color=colors[i])
@@ -150,7 +192,7 @@ def plot_problem_statistics(
 
     # Plot Coefficient of Variation
     for i in range(len(strategies)):
-        ax3.bar(index + i * bar_width, var_time_values[:, i], bar_width, label=strategies[i], color=colors[i])
+        ax3.bar(index + i * bar_width, cv_time_values[:, i], bar_width, label=strategies[i], color=colors[i])
 
     ax3.set_xlabel('Problem-Language')
     ax3.set_ylabel('Coefficient of Variation')
@@ -248,7 +290,7 @@ def main(
         {}
     )  # strategy -> problem_id -> language -> (avg_time, max_time)
 
-    strategies = ["alphacode", "feedback_diff_solution", "feedback_diff_input", "feedback_multi_solution_diff_input", "multi_solution_diff_input", "time_contrast", "plain_problem", "slow_solution", "diff_solution_one_input", "random_solution", "evalperf_slow_solution", "evalperf_random_solution"]
+    strategies = ["alphacode", "feedback_diff_solution", "feedback_diff_input", "feedback_multi_solution_diff_input", "multi_solution_diff_input", "time_contrast", "plain_problem", "slow_solution", "diff_solution_one_input", "random_solution", "evalperf_slow_solution", "evalperf_random_solution", "plain_problem_contract", "evalperf_slow_solution_contract", "evalperf_random_solution_contract", "feedback_diff_input_contract"]
     for strategy in strategies:  # experiment_name
         experiment_dir = Path("results") / strategy
         experiment_statistics[strategy] = {}
@@ -313,7 +355,7 @@ def main(
     for strategy in strategies:
         for problem_id, mean_solutions_data in experiment_statistics[strategy].items():
             for language, times in mean_solutions_data.items():
-                avg_time, max_time, var_time = times
+                avg_time, max_time, cv_time = times
                 if problem_id not in problem_longest_strategies:
                     problem_longest_strategies[problem_id] = {}
                 if language not in problem_longest_strategies[problem_id]:
@@ -326,7 +368,7 @@ def main(
                     (
                         longest_avg_time_strategy,
                         longest_max_time_strategy,
-                        largest_var_time_strategy,
+                        largest_cv_time_strategy,
                     ) = problem_longest_strategies[problem_id][language]
 
                     if (
@@ -344,17 +386,17 @@ def main(
                     ):
                         longest_max_time_strategy = strategy
                     if (
-                        var_time
-                        > experiment_statistics[largest_var_time_strategy][problem_id][
+                        cv_time
+                        > experiment_statistics[largest_cv_time_strategy][problem_id][
                             language
                         ][2]
                     ):
-                        largest_var_time_strategy = strategy
+                        largest_cv_time_strategy = strategy
 
                     problem_longest_strategies[problem_id][language] = (
                         longest_avg_time_strategy,
                         longest_max_time_strategy,
-                        largest_var_time_strategy,
+                        largest_cv_time_strategy,
                     )
 
     pp = pprint.PrettyPrinter(depth=4, width=90)
@@ -365,13 +407,15 @@ def main(
     # print("Experiment statistics:")
     # pp.pprint(experiment_statistics)
     print(
-        "Problem statistics: problem_id -> language -> strategy -> (avg_time, max_time, var_time)"
+        "Problem statistics: problem_id -> language -> strategy -> (avg_time, max_time, cv_time)"
     )
     pp.pprint(problem_statistics)
 
     plot_problem_statistics(problem_statistics, strategies)
 
     plot_avg_over_problem(problem_statistics, strategies)
+
+    plot_violin_plot(problem_statistics, strategies)
 
 if __name__ == "__main__":
     Fire(main)
